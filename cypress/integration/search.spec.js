da4be492f4d13ff66ddd2bcc5f309e80b009f76c
Israel Reyes <israel.reyes@encora.com>
2021-07-13 18:39:18 -0500

    Solved T-1 and T-2

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,7 +1,27 @@
 /// <reference types="Cypress" />
 
-describe('Search elements', ()=>{
-    it('Search for elementswit multiple results', ()=>{
+describe('Search elements', () => {
+    beforeEach(()=>{
         cy.visit('/')
+    })
+    
+    it('Search for elements with multiple results', () => {
+        cy.fixture('index').then(data => {
+            cy.get(data.searchBox).type('dress')
+            cy.get(data.searchButton).click()
+        })
+        cy.fixture('searchResult').then(data => {
+            cy.get(data.title).should('contain', 'dress')
+        })
+    })
+
+    it('Search for elements with no results', ()=>{
+        cy.fixture('index').then(data => {
+            cy.get(data.searchBox).type('qwerty')
+            cy.get(data.searchButton).click()
+        })
+        cy.fixture('searchResult').then(data => {
+            cy.get(data.alert).should('contain', 'No results were found for your search')
+        })
     })
 })
